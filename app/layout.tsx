3dd951f7b7b8d416c9c6f80d6e09ad3e78f50741
Yusuf Mustahan <yumustyology@gmail.com>
2024-07-21 06:03:48 -0700

    revamp

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en"> 
       <body className={cn(inter.className)}>{children}</body>
     </html>
   );
